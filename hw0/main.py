d5f6ea31584cb67903bc544531131b8c9efe4fec
Julian Cooper <jelc@stanford.edu>
2022-01-07 00:19:26 +1100

    Progressed logic to solve for answer, tbd hex conversion

--- hw0/main.py
+++ hw0/main.py
@@ -1,5 +1,7 @@
-import os
+import collections
+import string
 import sys
+import os
 
 def strxor(a, b):     # xor two strings (trims the longer input)
     return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b)])
@@ -19,19 +21,60 @@ def main():
 
     # load ciphertext data
     with open(data_path, 'r') as f:
-        ciphertext = [c.strip() for c in f.readlines()]
+        ciphers = [str(c.strip()) for c in f.readlines()]
+        target = ciphers.pop()
 
-    # xor paris of cipher text msgs
-    m01 = strxor(ciphertext[0], ciphertext[1])
-    m23 = strxor(ciphertext[2], ciphertext[3])
-    m45 = strxor(ciphertext[4], ciphertext[5])
-    m67 = strxor(ciphertext[6], ciphertext[7])
-    m89 = strxor(ciphertext[8], ciphertext[9])
-    
-    print(ciphertext[0])
-    
-    
+    # xor pairs of cipher text 
+    final_key = [None]*150
+    known_key_positions = set()
 
+    for i0, c0 in enumerate(ciphers):
+        # c0_ascii = bytearray.fromhex(c0).decode("latin1")
+        counter = collections.Counter()
+
+        # for each other ciphertext
+        for i1, c1 in enumerate(ciphers):
+            if i0 != i1:  # don't xor a ciphertext with itself
+                # c1_ascii = bytearray.fromhex(c1).decode("latin1")
+                m01 = strxor(c0, c1)
+                for idxOfChar, char in enumerate(m01):
+                    # if a character in xored result is alphanumeric char, there was probably a space character in one of the plaintexts
+                    if char in string.printable and char.isalpha(): counter[idxOfChar] += 1  # increment counter at this index
+
+        # find all positions where space character likely in i0 cipher
+        knownSpaceIndexes = []
+        for ind, val in counter.items():
+            # if space found at least 7 times at this index out of 9 possible xors, then likely from i0 cipher
+            if val >= 7: knownSpaceIndexes.append(ind)
+        
+        # xor current_index with spaces
+        xor_with_spaces = strxor(c0,' '*150)
+
+        for index in knownSpaceIndexes:
+            # store key's value at the correct position
+            final_key[index] = xor_with_spaces[index].encode('utf-8')
+
+            # record that we know the key at this position
+            known_key_positions.add(index)
+    print(final_key)
+    # construct a hex key from the currently known key, adding in '00' hex chars where we do not know (to make a complete hex string)
+    final_key_hex = ''.join([val.decode('utf-8') if val is not None else '00' for val in final_key])
+
+    # xor the currently known key with the target cipher
+    target_ascii = bytearray.fromhex(target).decode("latin1")
+    final_key_ascii = bytearray.fromhex(final_key_hex).decode("latin1")
+    output = strxor(target_ascii, final_key_ascii)
+
+    # print the output, printing a * if that character is not known yet
+    print(''.join([char if index in known_key_positions else '*' for index, char in enumerate(output)]))
+
+    # We then confirm this is correct by producing the key from this, and decrpyting all the other messages to ensure they make grammatical sense
+    # target_plaintext = "The secret message is: When using a stream cipher, never use the key more than once"
+    # print(target_plaintext)
+    # key = strxor(bytearray.fromhex(target).decode('latin1'),target_plaintext)
+    # for cipher in ciphers:
+    #     print(strxor(bytearray.fromhex(cipher).decode('latin1'),key))
+    
 
 if __name__ == '__main__':
     main()
