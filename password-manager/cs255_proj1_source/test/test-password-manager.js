c3bdccfb05ab06fad8c86ddd79f9b4f6bc4d8a89
Julian Cooper <jelc@stanford.edu>
2022-01-31 15:15:46 -0800

    Minor bug fix for test.js script as per ED #259

--- password-manager/cs255_proj1_source/test/test-password-manager.js
+++ password-manager/cs255_proj1_source/test/test-password-manager.js
@@ -3,7 +3,7 @@
 var expect = require('expect.js');
 const { Keychain } = require('../password-manager');
 
-describe('Password manager', async function() {
+describe('Password manager', async function () {
     this.timeout(5000);
     var password = "password123!";
 
@@ -13,13 +13,13 @@ describe('Password manager', async function() {
         "service3": "value3"
     };
 
-    describe('functionality', async function() {
+    describe('functionality', async function () {
 
-        it('inits without an error', async function() {
+        it('inits without an error', async function () {
             await Keychain.init(password);
         });
 
-        it('can set and retrieve a password', async function() {
+        it('can set and retrieve a password', async function () {
             let keychain = await Keychain.init(password);
             var url = 'www.stanford.edu';
             var pw = 'sunetpassword';
@@ -27,7 +27,7 @@ describe('Password manager', async function() {
             expect(await keychain.get(url)).to.equal(pw);
         });
 
-        it('can set and retrieve multiple passwords', async function() {
+        it('can set and retrieve multiple passwords', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -37,7 +37,7 @@ describe('Password manager', async function() {
             }
         });
 
-        it('returns null for non-existent passwords', async function() {
+        it('returns null for non-existent passwords', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -45,7 +45,7 @@ describe('Password manager', async function() {
             expect(await keychain.get('www.stanford.edu')).to.be(null);
         });
 
-        it('can remove a password', async function() {
+        it('can remove a password', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -54,7 +54,7 @@ describe('Password manager', async function() {
             expect(await keychain.get('service1')).to.be(null);
         });
 
-        it('returns false if there is no password for the domain being removed', async function() {
+        it('returns false if there is no password for the domain being removed', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -62,7 +62,7 @@ describe('Password manager', async function() {
             expect(await keychain.remove('www.stanford.edu')).to.be(false);
         });
 
-        it('can dump and restore the database', async function() {
+        it('can dump and restore the database', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -73,15 +73,15 @@ describe('Password manager', async function() {
             var newKeychain = await Keychain.load(password, contents, checksum);
 
             // Make sure it's valid JSON
-            expect(async function() {
+            expect(async function () {
                 JSON.parse(contents)
             }).not.to.throwException();
             for (var k in kvs) {
-                expect(await keychain.get(k)).to.equal(kvs[k]);
+                expect(await newKeychain.get(k)).to.equal(kvs[k]);
             }
         });
 
-        it('fails to restore the database if checksum is wrong', async function() {
+        it('fails to restore the database if checksum is wrong', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -89,10 +89,10 @@ describe('Password manager', async function() {
             var data = await keychain.dump();
             var contents = data[0];
             var fakeChecksum = '3GB6WSm+j+jl8pm4Vo9b9CkO2tZJzChu34VeitrwxXM=';
-            await expect(async function() { Keychain.load(password, contents, fakeChecksum)}).rejects;
+            await expect(async function () { Keychain.load(password, contents, fakeChecksum) }).rejects;
         });
 
-        it('returns false if trying to load with an incorrect password', async function() {
+        it('returns false if trying to load with an incorrect password', async function () {
             let keychain = await Keychain.init(password);
             for (var k in kvs) {
                 await keychain.set(k, kvs[k]);
@@ -100,14 +100,14 @@ describe('Password manager', async function() {
             var data = await keychain.dump();
             var contents = data[0];
             var checksum = data[1];
-            expect(async function() {await Keychain.load("fakepassword", contents, data[1])}).rejects;
+            expect(async function () { await Keychain.load("fakepassword", contents, data[1]) }).rejects;
         });
     });
 
-    describe('security', async function() {
+    describe('security', async function () {
 
         // Very basic test to make sure you're not doing the most naive thing
-        it("doesn't store domain names and passwords in the clear", async function() {
+        it("doesn't store domain names and passwords in the clear", async function () {
             let keychain = await Keychain.init(password);
             var url = 'www.stanford.edu';
             var pw = 'sunetpassword';
@@ -122,7 +122,7 @@ describe('Password manager', async function() {
         // This test won't be graded directly -- it just exists to make sure your
         // dump include a kvs object with all your urls and passwords, because
         // we will be using that in other tests.
-        it('includes a kvs object in the serialized dump', async function() {
+        it('includes a kvs object in the serialized dump', async function () {
             let keychain = await Keychain.init(password);
             for (var i = 0; i < 10; i++) {
                 await keychain.set(String(i), String(i));
